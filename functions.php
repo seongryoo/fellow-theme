9b56a0174a7b7a5f4d313b974ee04fa23516e50b
Seong-Hyun Ryoo <seongryoo@gmail.com>
2025-06-29 13:30:30 -0400

    Initial commit with bumbo

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function fellow_app_query_vars($qvars): array
+{
+    $qvars[] = 'member';
+    return $qvars;
+}
+
+add_filter('query_vars', 'fellow_app_query_vars');
+
+add_action('init', 'add_author_rules');
+function add_author_rules()
+{
+    add_rewrite_rule(
+        'member/([^/]+)/?$',
+        'index.php?pagename=profile&member=$matches[1]',
+        'top'
+    );
+
+}
+
+?>
---
+++ page-profile.php
@@ -0,0 +1,20 @@
+<?php
+
+$member_obj = false;
+$member_username = get_query_var('member');
+
+if ($member_username != '') {
+    $member_obj = get_user_by('slug', $member_username);
+}
+
+?>
+
+<?php if ($member_obj == false): ?>
+    <h1>Looks like this member doesn't exist. :(</h1>
+
+<?php else:
+    $member_name = $member_obj->display_name;
+    ?>
+    <h1>Member: <?php echo $member_name ?></h1>
+
+<?php endif; ?>
